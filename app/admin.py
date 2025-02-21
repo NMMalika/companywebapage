90d970a60b4b620fe793538a54904825b8b1bfed
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-02-21 18:11:56 +0300

    intergrating bootstrap template with Django

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import GeneralInfo
 
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+    list_display = ['company_name','location','email','phone','video_url','facebook','twitter','linkedin','instagram']
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class GeneralInfo(models.Model):
+    company_name = models.CharField(max_length=100 , default='Nimcity Enterprises')
+    location = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    video_url = models.URLField(blank=True,null=True)
+    facebook = models.URLField(blank=True,null=True)
+    twitter = models.URLField(blank=True,null=True)
+    linkedin = models.URLField(blank=True,null=True)
+    instagram = models.URLField(blank=True,null=True)
+    
+
+    def __str__(self):
+        return self.company_name
